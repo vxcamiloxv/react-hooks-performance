73d8f409f5df8d37518ad409bd2b3fad8b484233
Tremerex <robert.gomezc@gmail.com>
2020-08-29 20:38:49 -0500

    feat(mater): film and species components

---
+++ components/CharacterSpecies/CharacterSpecies.style.js
@@ -0,0 +1,105 @@
+import { css } from 'styled-components';
+
+export default {
+  container: css`
+    border-top-right-radius: 10px;
+    border-bottom-left-radius: 10px;
+    cursor: pointer;
+    overflow: hidden;
+    padding: 6px 6px;
+    position: relative;
+    transition: all .5s;
+    width: 360px;
+
+    &:after, &:before {
+      border : 0px solid #fff;
+      content: "";
+      height: 10px;
+      position: absolute;
+      transition: all .5s;
+      width: 10px;
+    }
+
+    &:after {
+      border-left: 5px solid black;
+      border-top: 5px solid black;
+      left: 0;
+      top: 0;
+    }
+
+    &:before {
+      border-bottom: 5px solid black;
+      border-right: 5px solid black;
+      bottom: 0;
+      right: 0;
+    }
+
+    &:hover {
+      border-bottom-left-radius: 0px;
+      border-top-right-radius: 0px;
+
+      & .figure {
+        background: #1abc9c;
+
+        & .image {
+          opacity: .2;
+        }
+      }
+
+      & .section {
+        bottom: 0;
+        opacity: 1;
+      }
+
+      &:before, &:after {
+        box-sizing: border-box;
+        height: 100%;
+        width: 100%;
+      }
+    }
+
+    & .figure {
+      display: flex;
+      margin: 0;
+      padding: 0;
+      width: 100%;
+    }
+
+    & .image {
+      width: 100%;
+    }
+
+    & .section {
+      box-sizing: border-box;
+      bottom: 100px;
+      color: black;
+      height: 100%;
+      margin: 0;
+      left: 0px;
+      opacity: 0;
+      padding: 12px 12px;
+      position: absolute;
+      transition: .3s ease-in-out;
+      width: 100%;
+
+      & .title {
+        font-size: 1em;
+        font-weight: 700;
+        padding-bottom: 2px;
+        text-align: center;
+      }
+
+      & .sub {
+        color: #262626;
+        font-size: .9em;
+        font-weight: 600;
+        line-height: 0.5;
+        margin: 12px 0 12px;
+      }
+
+      & .text {
+        font-size: .8em;
+      }
+    }
+  `,
+};
--- components/CharacterSpecies/index.js
+++ components/CharacterSpecies/index.js
@@ -1,1 +1,83 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+import Styles from './CharacterSpecies.style';
+
+const Container = styled.div`${Styles.container}`;
+
+const IMAGE_PATH = 'https://starwars-visualguide.com/assets/img/species/';
+const IMAGE_EXT = '.jpg';
+
+const characterSpecies = ({
+  id, name, classification, designation, averageLifespan, averageHeight, language, skinColors, hairColors, eyeColors, homeWorld
+}) => {
+  const imageUrl = `${IMAGE_PATH}${id}${IMAGE_EXT}`;
+
+  return (
+    <>
+      <Container>
+        <figure class="figure">
+          <img class="image" src={imageUrl} alt={name} />
+        </figure>
+        <div class="section">
+          <h2 class="title">{name}</h2>
+          <div>
+            <h3 class="sub">Classification</h3>
+            <div class="text">{classification}</div>
+          </div>
+          <div>
+            <h3 class="sub">Designation</h3>
+            <div class="text">{designation}</div>
+          </div>
+          <div>
+            <h3 class="sub">Avg Lifespan</h3>
+            <div class="text">{averageLifespan}</div>
+          </div>
+          <div>
+            <h3 class="sub">Avg Height</h3>
+            <div class="text">{averageHeight}</div>
+          </div>
+          <div>
+            <h3 class="sub">Language</h3>
+            <div class="text">{language}</div>
+          </div>
+          <div>
+            <h3 class="sub">Skin Color(s)</h3>
+            <div class="text">{skinColors}</div>
+          </div>
+          <div>
+            <h3 class="sub">Hair Color(s)</h3>
+            <div class="text">{hairColors}</div>
+          </div>
+          <div>
+            <h3 class="sub">Eye Color(s)</h3>
+            <div class="text">{eyeColors}</div>
+          </div>
+          <div>
+            <h3 class="sub">Home World</h3>
+            <div class="text">{homeWorld}</div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+
+
+};
+
+characterSpecies.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  classification: PropTypes.string,
+  designation: PropTypes.string,
+  averageHeight: PropTypes.string,
+  skinColors: PropTypes.string,
+  hairColors: PropTypes.string,
+  eyeColors: PropTypes.string,
+  averageLifespan: PropTypes.string,
+  homeWorld: PropTypes.string,
+  language: PropTypes.string,
+};
+
+export default characterSpecies;
